b02dbf9c2064562f040fbb3c1121e761363ad71f
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-01-24 23:05:33 +0530

    Trying merge sort

--- Assignments/DSA/A19/MergeSortRecursion.cpp
+++ Assignments/DSA/A19/MergeSortRecursion.cpp
@@ -42,7 +42,62 @@ void printArray(int *array, int size)
     std::cout<<"\n";
 }
 
+void mergeArrays(int *array, int leftOfFirstArray, int rightOfFirstArray, int leftOfSecondArray, int rightOfSecondArray, int size1, int size2)
+{
+
+    int newArrayIndex = 0, newArraySize = size1 + size2, indexOfFirstArray = leftOfFirstArray, indexOfSecondArray = leftOfSecondArray;
+    int *newArray = new int[newArraySize];
+
+    while ( indexOfFirstArray < size1 && indexOfSecondArray <= size2  )
+    {
+        if ( array[indexOfFirstArray] < array[indexOfSecondArray]  )
+        {
+            newArray[newArrayIndex] = array[indexOfFirstArray];
+            indexOfFirstArray++;
+        }
+        else
+        {
+            newArray[newArrayIndex] = array[indexOfSecondArray];
+            indexOfSecondArray++;
+        }
+        newArrayIndex++;
+    }
+
+    while ( indexOfFirstArray < size1 )
+    {
+        newArray[newArrayIndex] = array[indexOfFirstArray];
+        indexOfFirstArray++;
+        newArrayIndex++;
+    }
+
+    while ( indexOfSecondArray <= size2 )
+    {
+        newArray[newArrayIndex] = array[indexOfSecondArray];
+        indexOfSecondArray++;
+        newArrayIndex++;
+    }
+
+    int left = leftOfFirstArray;
+
+    for ( newArrayIndex = 0 ; newArrayIndex < newArraySize && left <= rightOfSecondArray ; newArrayIndex++, left++ )
+    {
+        array[left] = newArray[newArrayIndex];
+    }
+
+    delete newArray;
+    
+}
+
 void mergeSort(int *array, int left, int right)
 {
-    
+    if ( left >= right || left < 0 || right < 0 )
+        return;
+
+    int middle = ( left + right ) / 2;
+
+    mergeSort( array, left, middle );
+    mergeSort( array, middle + 1, right );
+
+    int size1 = left + right, size2 = left + right;
+    mergeArrays(array, left, middle, middle + 1, right, size1, size2);
 }
