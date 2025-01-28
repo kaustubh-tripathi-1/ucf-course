6a734158d0ecb9027c30519c2f9977dd171754b3
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-01-28 13:13:42 +0530

    Implement Binary Search

---
+++ Assignments/DSA/A21/BinarySearch.cpp
@@ -0,0 +1,117 @@
+#include<iostream>
+#include<vector>
+
+void printData(const std::vector<int> &nums)
+{
+    if ( nums.size() < 1)
+        return;
+
+    for ( int i = 0 ; i < nums.size() ; i++ )
+        std::cout<<nums.at(i)<<" ";
+
+    std::cout<<"\n";
+}
+
+//$ Hoare's partition which is best
+int hoarePartition(std::vector<int> &array, int left, int right)
+{
+
+    int pivot = array[left];
+    int leftPtr = left - 1, rightPtr = right + 1;
+
+    while ( true )
+    {
+        do
+        {
+            leftPtr++;
+        } while ( pivot > array[leftPtr] );
+
+        do
+        {
+            rightPtr--;
+        } while ( pivot < array[rightPtr]  );
+
+        if ( leftPtr >= rightPtr )
+            return rightPtr;
+
+        std::swap( array[leftPtr], array[rightPtr] );
+
+    }
+
+}
+
+//@ Sorts the array using Quick Sort Algo.
+void quickSort(std::vector<int> &array, int left, int right)
+{
+    if ( left >= right )
+        return;
+
+    //$ For hoare partition
+    int pivotIndex = hoarePartition(array, left, right);
+    quickSort(array, left, pivotIndex );
+    quickSort(array, pivotIndex + 1, right);
+
+}
+
+//@ Binary Search
+size_t binarySearch(const std::vector<int> &nums, int target, size_t left, size_t right)
+{
+    //& Recursive approach
+    /* if ( left > right )
+        return -1;
+    
+    size_t middle = left + ( right - left ) / 2;
+    if ( target == nums[middle])
+        return middle;
+    else if ( target < nums[middle])
+        return binarySearch(nums, target, left, middle - 1);
+    else
+        return binarySearch(nums, target, middle + 1, right);
+
+    return -1; */
+
+    //& Iterative approach
+    if ( left > right )
+        return -1;
+
+    while ( left <= right )
+    {
+        size_t middle = left + ( right - left ) / 2;
+
+        if ( target == nums[middle] )
+            return middle;
+        else if ( target < nums[middle] )
+            right = middle - 1;
+        else
+            left = middle + 1;
+    }
+
+    return -1;
+        
+}
+
+int main()
+{
+    std::vector <int> nums = {22, 432, 546, 7678, 87987, 54 ,564 ,42, 65, -32443, 43, -214, 0};    
+
+    int target = 7678;
+
+    std::cout<<"Sorting using Quick Sort -\n\n";
+    std::cout<<"Data before sorting -\n";
+    printData(nums);
+
+    quickSort(nums, 0, nums.size() - 1);
+
+    std::cout<<"\nData after sorting -\n";
+    printData(nums);
+    
+    size_t result = binarySearch(nums, target, 0, (nums.size() - 1 ));
+
+    if ( result != -1 )
+        std::cout<<"\n\nThe target element "<<target<<" is present in the data set and has index : "<<result<<"\n";
+    else
+        std::cout<<"\n\nThe target "<<target<<" is not present in the data set."<<"\n";
+
+    std::cin.get();
+    return 0;
+}
