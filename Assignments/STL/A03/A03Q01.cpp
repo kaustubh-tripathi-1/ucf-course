d3ab8ae69990f63a0d0ebf26482aaaa09d485585
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-02-05 05:22:23 +0530

    Solve Q1 of std::array

---
+++ Assignments/STL/A03/A03Q01.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<array>
+
+int main()
+{
+    std::array<int, 5> arr = {3, 4, 9, 5, 21};
+
+    std::array<int, 5>::reverse_iterator i;
+
+    std::cout<<"Printing elements in reverse :\n";
+    for ( i = arr.rbegin() ; i != arr.rend() ; i++ )
+    {
+        std::cout<<*i<<" ";
+    }
+
+    std::cin.get();
+    return 0;
+}
