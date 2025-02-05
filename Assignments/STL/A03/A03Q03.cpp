ec4fb4b05200841f980adaa35964795e559ae9b6
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-02-06 01:49:08 +0530

    Use std::max_element

--- Assignments/STL/A03/A03Q03.cpp
+++ Assignments/STL/A03/A03Q03.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<array>
+#include<algorithm>
 
 int main()
 {
@@ -7,17 +8,20 @@ int main()
     
     std::array<int, 10>::iterator i;
 
+    std::cout<<"Enter 10 elements to find the max :\n";
     for ( i = arr.begin() ; i != arr.end() ; i++ )
     {
         std::cin>>*i;
     }
 
-    auto max = arr.front();
+    /* auto max = arr.front();
     for ( auto ele : arr )
     {
         if ( max < ele )
             max = ele;
-    }
+    } */
+
+    auto max = *std::max_element(arr.begin(), arr.end());
 
     std::cout<<"Largest element is : "<<max<<"\n";
     
